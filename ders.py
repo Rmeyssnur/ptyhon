24fa2c08549c8d933aa2f6ce373f99656e3f36c9
Rmeyssnur <rumfth@gmail.com>
2025-10-19 21:30:54 +0300

    Add files via upload

---
+++ ders.py
@@ -0,0 +1,30 @@
+
+print("Python ")
+
+isim = "Rümeysa"
+print(f"selam {isim}")
+
+x, y = "kitap", "defter"
+print(x + " ve " + y)
+
+r = 77
+a= 1177
+print(r + a)
+
+c = d = 42
+print(c, d)
+
+bool_deger = True
+print(type(bool_deger))
+
+x = 17.77
+print(int(x))
+
+metin = "Samsun denizi"
+print("Samsun" in metin)
+
+kelime = "metin"
+print(kelime.lower())
+
+puan = 75.50
+print(f"defterin fıyatı: {puan:.2f}")
